0d66a59f72aae401346100aa2af32e3f75cbb72a
Rashid usmani <rashidusmani506@gmail.com>
2026-04-25 20:05:47 +0530

    Polymorphism

--- One_Basic_OOP/L4_Polymorphism.java
+++ One_Basic_OOP/L4_Polymorphism.java
@@ -1,8 +1,26 @@
 package One_Basic_OOP;
 
+class MathOperations{
+    void add(int a, int b,int c){
+        System.out.println(a + b + c);
+    }
+
+     void add(int a, int b){
+        System.out.println(a + b );
+    }
+
+     void add(Double a, Double b){
+        System.out.println(a + b );
+    }
+
+     void add(Double a, Double b,Double c){
+        System.out.println(a + b + c);
+    }
+}
 public class L4_Polymorphism {
 
     public static void main(String[] args) {
-        
+        MathOperations obj = new MathOperations();
+        obj.add(2.8,3.7);
     }
 }
