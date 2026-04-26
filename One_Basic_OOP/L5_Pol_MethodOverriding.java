f38bdd63e85f7e6c7cb32d10b1e07c2cc3ad3bcd
Rashid usmani <rashidusmani506@gmail.com>
2026-04-26 16:06:32 +0530

    Polymorphisim method overriding

---
+++ One_Basic_OOP/L5_Pol_MethodOverriding.java
@@ -0,0 +1,33 @@
+package One_Basic_OOP;
+class Shape{
+    void draw(){
+        System.out.println("Shape is drawn");
+    }
+}
+
+class Circle extends Shape{
+    @Override
+    void draw(){
+        super.draw();
+        System.out.println("Circle is drawn");
+    }
+}
+
+class Rectangle extends Shape{
+    @Override
+    void draw(){
+        System.out.println("Rectangle is drawn");
+    }
+}
+public class L5_Pol_MethodOverriding {
+    public static void main(String[] args) {
+        Shape obj = new Rectangle();
+        obj.draw();
+
+        Shape obj2 = new Circle();
+        obj2.draw();
+
+        Shape obj3 = new Shape();
+        obj3.draw();
+    }
+}
