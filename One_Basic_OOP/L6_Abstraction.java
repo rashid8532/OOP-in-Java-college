b73cd38bb25e19ad0576836fd319a304a203d6ae
Rashid usmani <rashidusmani506@gmail.com>
2026-04-26 21:34:24 +0530

    Abstraction

--- One_Basic_OOP/L6_Abstraction.java
+++ One_Basic_OOP/L6_Abstraction.java
@@ -1,7 +1,34 @@
 package One_Basic_OOP;
 
+abstract class Vehical {
+
+    abstract void Start();
+}
+
+class Bike extends Vehical {
+    @Override
+    void Start(){
+        System.out.println("Bike starts with the kick ");
+    }
+}
+
+class Car extends Vehical {
+    @Override
+    void Start(){
+        System.out.println("Car starts with the key ");
+    }
+}
 
 public class L6_Abstraction {
     
-    
+   public static void main(String[] args) {
+
+    Vehical V1 = new Car();
+    Vehical V2 = new Bike();
+
+    V1.Start();
+    V2.Start();
+
+   }
+
 }
