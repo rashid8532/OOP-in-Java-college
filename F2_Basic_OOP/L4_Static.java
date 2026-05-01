1d6ada75200b47edb794d3578399b320a00d0e93
Rashid usmani <rashidusmani506@gmail.com>
2026-05-01 18:51:59 +0530

    Static Keyword

--- F2_Basic_OOP/L4_Static.java
+++ F2_Basic_OOP/L4_Static.java
@@ -2,13 +2,23 @@ package F2_Basic_OOP;
 class Employee{
     String name ;
     int ID ;
-    static String company = "TCS";
+    static String company = "TCS";//this line of code can be run without object 
+    
 
+    // And if i made static block { // this part of code will run with out the call }
+    // static{
+    //     System.out.println("Rashid is good boy");
+    // }
+
+
+    
     Employee(String name, int ID){
         this.name = name;
         this.ID = ID;
     }
 }
+
+
 public class L4_Static {
     public static void main(String[] args) {
         Employee e1 = new Employee("Rashid", 123);
