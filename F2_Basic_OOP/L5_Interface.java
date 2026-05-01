76cffc3e213223fa21abbab7b4164c37d16fc0f2
Rashid usmani <rashidusmani506@gmail.com>
2026-05-01 19:49:18 +0530

    Interface

--- F2_Basic_OOP/L5_Interface.java
+++ F2_Basic_OOP/L5_Interface.java
@@ -1,5 +1,28 @@
 package F2_Basic_OOP;
 
+interface Payment {
+    void pay();
+}
+
+class UPI implements Payment{
+    @Override
+    public void pay(){
+        System.out.println("Paid using UPI");
+    }
+}
+
+class CreditCard implements Payment{
+    @Override
+    public void pay(){
+        System.out.println("Paid using CreditCard");
+    }
+}
 public class L5_Interface {
-    
+    public static void main(String[] args) {
+        UPI p1 = new UPI();
+        CreditCard p2 = new CreditCard();
+
+        p1.pay();
+        p2.pay();
+    }
 }
